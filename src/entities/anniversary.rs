ef4deb7cd6f2aa13bcf79783a1e46cb4542d2575
Mael Sicard-Cras <mael.sicardcras@gmail.com>
2022-01-19 00:28:07 +0100

    formatting & playing around lib settings

--- src/entities/anniversary.rs
+++ src/entities/anniversary.rs
@@ -1,38 +1,38 @@
 use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone)]
-pub struct Anniversary{
+pub struct Anniversary {
     pub name: String,
     pub count: u32,
     pub unit: String,
-    pub date:  DateTime<Utc>
+    pub date: DateTime<Utc>,
 }
 
-impl Anniversary{
-    pub fn new_seconds(count: u32, date:  DateTime<Utc>) -> Anniversary{
-        Anniversary{
+impl Anniversary {
+    pub fn new_seconds(count: u32, date: DateTime<Utc>) -> Anniversary {
+        Anniversary {
             count,
             date,
             name: String::from(format!("{} seconds", count)),
-            unit: String::from("seconds")
+            unit: String::from("seconds"),
         }
     }
 
-    pub fn new_days(count: u32, date:  DateTime<Utc>) -> Anniversary{
-        Anniversary{
+    pub fn new_days(count: u32, date: DateTime<Utc>) -> Anniversary {
+        Anniversary {
             count,
             date,
             name: String::from(format!("{} days", count)),
-            unit: String::from("days")
+            unit: String::from("days"),
         }
     }
 
-    pub fn new_weeks(count: u32, date:  DateTime<Utc>) -> Anniversary{
-        Anniversary{
+    pub fn new_weeks(count: u32, date: DateTime<Utc>) -> Anniversary {
+        Anniversary {
             count,
             date,
             name: String::from(format!("{} weeks", count)),
-            unit: String::from("weeks")
+            unit: String::from("weeks"),
         }
     }
 }
--- test/test.rs
+++
@@ -1,7 +0,0 @@
-// use lib::entities::anniversary::Anniversary;
-
-fn main() {
-    let date: &str = "1989-06-19T00:00:00.000Z";
-    let anniversaries = lib::find_anniversaries_future(date);
-    anniversaries.for_each(|a| println!(format!("{}: {}", a.name, a.date)));
-}
